7751c25e15f01db302b089372821e46487591564
heixiansen <leizi97@126.com>
2025-02-17 15:10:22 +0800

    增加图标导出库

---
+++ composables/icon.ts
@@ -0,0 +1,34 @@
+import { basename, extname } from "@tauri-apps/api/path";
+import { exists } from "@tauri-apps/plugin-fs";
+import { Command } from "@tauri-apps/plugin-shell";
+
+export const getIcon = async function (sourcePath: string, icoDirPath: string) {
+	if(sourcePath.endsWith(".url")) return false;
+	let res = await Command.create("ResourcesExtract", [
+		"/Source",
+		sourcePath,
+		"/DestFolder",
+		icoDirPath,
+		"/ExtractIcons",
+		"1",
+		"/ExtractCursors",
+		"0",
+		"/OpenDestFolder",
+		"0",
+		"/MultiFilesMode",
+		"1"
+	]).execute();
+	if (res.code == 0) {
+		let icoPath = icoDirPath + "\\" + (await basename(sourcePath));
+		icoPath = icoPath.replace(`.${await extname(sourcePath)}`, "_1.ico");
+		console.log(icoPath)
+		if (await exists(icoPath)) {
+			return icoPath;
+		} else {
+			return false;
+		}
+		return;
+	} else {
+		return false;
+	}
+};

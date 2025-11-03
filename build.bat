6b373a3ed643c07fa285c85864c448562cf28a56
黑先森 <leizi97@126.com>
2025-11-03 16:13:50 +0800

    #Update 优化构建流程

--- build.bat
+++ build.bat
@@ -1,12 +1,20 @@
 pnpm run build
 rustup default nightly
-@REM rem 使用rust nightly构建支持win7的版本，请先将根目录的windows.0.48.5 放入以下目录
-@REM rem C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\你自己的版本\lib\x64
-@REM rem C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\你自己的版本\lib\x86
-@REM rem C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\你自己的版本\atlmfc\lib\x64
-@REM rem C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\你自己的版本\atlmfc\lib\x86
+
 pnpm tauri build -- -Z  build-std --target x86_64-win7-windows-msvc
 rustup default stable
 pnpm tauri build
+
+@REM 复制文件夹src-tauri\easytier 到 src-tauri\target\release目录下 覆盖目录下同名目录
+xcopy /s /y /i src-tauri\easytier src-tauri\target\release\easytier
+@REM 复制 src-tauri\帮助.txt 和 src-tauri\uninstall.exe 到 src-tauri\target\release目录下 覆盖目录下同名文件
+xcopy /y src-tauri\帮助.txt src-tauri\target\release\帮助.txt
+xcopy /y src-tauri\uninstall.exe src-tauri\target\release\uninstall.exe
+@REM 复制文件夹src-tauri\easytier到 src-tauri\target\x86_64-win7-windows-msvc\release目录下 覆盖目录下同名目录
+@REM 复制 src-tauri\帮助.txt 和 src-tauri\uninstall.exe 到 src-tauri\target\x86_64-win7-windows-msvc\release目录下 覆盖目录下同名文件
+xcopy /y src-tauri\帮助.txt src-tauri\target\x86_64-win7-windows-msvc\release\帮助.txt
+xcopy /y src-tauri\uninstall.exe src-tauri\target\x86_64-win7-windows-msvc\release\uninstall.exe
+xcopy /s /y /i src-tauri\easytier src-tauri\target\x86_64-win7-windows-msvc\release\easytier
+
 pnpm run release
 pnpm run release-win7
